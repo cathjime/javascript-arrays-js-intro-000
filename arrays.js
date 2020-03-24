5c507e9317a281acb7581c561f8b0a5d7805729e
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-03-24 17:16:48 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,6 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 var addElementToBeginningOfArray = (array, element) => {
   return [element, ...array]
-  return newArr;
 }
 
 var destructivelyAddElementToBeginningOfArray = (array, element) => {
