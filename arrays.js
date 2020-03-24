a82497422295ad5e038993cc30e1856a8099d493
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-03-24 17:17:41 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,8 @@ var addElementToBeginningOfArray = (array, element) => {
 }
 
 var destructivelyAddElementToBeginningOfArray = (array, element) => {
-  return array.unshift(element);
+  array.unshift(element);
+  return array
 }
 
 function addElementToEndOfArray(array, element){
